7c5bd9c764512b6b2d01268ddad598b4479a502f
Sokwhan Huh <sokwhan@google.com>
2023-11-13 13:33:30 -0800

    Implement StructValue, TypeValue
    PiperOrigin-RevId: 582067363

---
+++ common/src/main/java/dev/cel/common/values/StructValue.java
@@ -0,0 +1,43 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cel.common.values;
+
+import com.google.errorprone.annotations.Immutable;
+
+/**
+ * StructValue is a representation of a structured object with typed properties.
+ *
+ * <p>Users may extend from this class to provide a custom struct that CEL can understand (ex:
+ * POJOs). Custom struct implementations must provide all functionalities denoted in the CEL
+ * specification, such as field selection, presence testing and new object creation.
+ */
+@Immutable
+public abstract class StructValue extends CelValue {
+
+  /**
+   * Performs field selection. For an expression `e` selecting a field `f`, `e.f` must throw an
+   * exception if `f` does not exist in the struct (i.e: hasField returns false). If the field
+   * exists but is not set, the implementation should return an appropriate default value based on
+   * the struct's semantics.
+   */
+  public abstract CelValue select(String fieldName);
+
+  /**
+   * Performs presence test on a field.
+   *
+   * @return true iff the field exists in the struct.
+   */
+  public abstract boolean hasField(String fieldName);
+}
---
+++ common/src/test/java/dev/cel/common/values/StructValueTest.java
@@ -0,0 +1,112 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cel.common.values;
+
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
+import com.google.testing.junit.testparameterinjector.TestParameterInjector;
+import com.google.testing.junit.testparameterinjector.TestParameters;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(TestParameterInjector.class)
+public class StructValueTest {
+
+  @Test
+  public void emptyStruct() {
+    UserDefinedClass userDefinedPojo = new UserDefinedClass(0);
+    CelCustomStruct celCustomStruct = new CelCustomStruct(userDefinedPojo);
+
+    assertThat(celCustomStruct.value()).isEqualTo(userDefinedPojo);
+    assertThat(celCustomStruct.isZeroValue()).isTrue();
+  }
+
+  @Test
+  public void constructStruct() {
+    UserDefinedClass userDefinedPojo = new UserDefinedClass(5);
+    CelCustomStruct celCustomStruct = new CelCustomStruct(userDefinedPojo);
+
+    assertThat(celCustomStruct.value()).isEqualTo(userDefinedPojo);
+    assertThat(celCustomStruct.isZeroValue()).isFalse();
+  }
+
+  @Test
+  public void selectField_success() {
+    UserDefinedClass userDefinedPojo = new UserDefinedClass(5);
+    CelCustomStruct celCustomStruct = new CelCustomStruct(userDefinedPojo);
+
+    assertThat(celCustomStruct.select("data")).isEqualTo(IntValue.create(5L));
+  }
+
+  @Test
+  public void selectField_nonExistentField_throws() {
+    UserDefinedClass userDefinedPojo = new UserDefinedClass(5);
+    CelCustomStruct celCustomStruct = new CelCustomStruct(userDefinedPojo);
+
+    assertThrows(IllegalArgumentException.class, () -> celCustomStruct.select("bogus"));
+  }
+
+  @Test
+  @TestParameters("{fieldName: 'data', expectedResult: true}")
+  @TestParameters("{fieldName: 'bogus', expectedResult: false}")
+  public void hasField_success(String fieldName, boolean expectedResult) {
+    UserDefinedClass userDefinedPojo = new UserDefinedClass(5);
+    CelCustomStruct celCustomStruct = new CelCustomStruct(userDefinedPojo);
+
+    assertThat(celCustomStruct.hasField(fieldName)).isEqualTo(expectedResult);
+  }
+
+  private static class UserDefinedClass {
+    private final long data;
+
+    private UserDefinedClass(long data) {
+      this.data = data;
+    }
+  }
+
+  @SuppressWarnings("Immutable") // Test only
+  private static class CelCustomStruct extends StructValue {
+    private final UserDefinedClass userDefinedClass;
+
+    @Override
+    public UserDefinedClass value() {
+      return userDefinedClass;
+    }
+
+    @Override
+    public boolean isZeroValue() {
+      return userDefinedClass.data == 0;
+    }
+
+    @Override
+    public CelValue select(String fieldName) {
+      if (fieldName.equals("data")) {
+        return IntValue.create(value().data);
+      }
+
+      throw new IllegalArgumentException("Invalid field name: " + fieldName);
+    }
+
+    @Override
+    public boolean hasField(String fieldName) {
+      return fieldName.equals("data");
+    }
+
+    private CelCustomStruct(UserDefinedClass value) {
+      this.userDefinedClass = value;
+    }
+  }
+}
