6d495272226be7e062b899782977c931a89b20ee
Justin King <jcking@google.com>
2024-08-05 13:43:23 -0700

    Open source Java conformance tests
    PiperOrigin-RevId: 659667787

---
+++ conformance/src/test/java/dev/cel/conformance/ConformanceTestRunner.java
@@ -0,0 +1,129 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cel.conformance;
+
+import com.google.api.expr.test.v1.SimpleProto.SimpleTest;
+import com.google.api.expr.test.v1.SimpleProto.SimpleTestFile;
+import com.google.api.expr.test.v1.SimpleProto.SimpleTestSection;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Splitter;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSortedMap;
+import com.google.protobuf.ExtensionRegistry;
+import com.google.protobuf.TextFormat;
+import com.google.protobuf.TypeRegistry;
+import java.io.BufferedReader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.runner.Description;
+import org.junit.runner.notification.RunNotifier;
+import org.junit.runners.ParentRunner;
+import org.junit.runners.model.InitializationError;
+import org.junit.runners.model.TestClass;
+
+public final class ConformanceTestRunner extends ParentRunner<ConformanceTest> {
+
+  private static final Splitter SPLITTER = Splitter.on(",").omitEmptyStrings();
+
+  private final ImmutableSortedMap<String, SimpleTestFile> testFiles;
+  private final ImmutableList<String> testsToSkip;
+
+  private static ImmutableSortedMap<String, SimpleTestFile> loadTestFiles() {
+    List<String> testPaths =
+        SPLITTER.splitToList(System.getProperty("dev.cel.conformance.ConformanceTests.tests"));
+    try {
+      TextFormat.Parser parser =
+          TextFormat.Parser.newBuilder()
+              .setTypeRegistry(TypeRegistry.getGeneratedTypeRegistry())
+              .build();
+      ImmutableSortedMap.Builder<String, SimpleTestFile> testFiles =
+          ImmutableSortedMap.naturalOrder();
+      for (String testPath : testPaths) {
+        SimpleTestFile.Builder fileBuilder = SimpleTestFile.newBuilder();
+        try (BufferedReader input =
+            Files.newBufferedReader(Paths.get(testPath), StandardCharsets.UTF_8)) {
+          parser.merge(input, ExtensionRegistry.getGeneratedRegistry(), fileBuilder);
+        }
+        SimpleTestFile testFile = fileBuilder.build();
+        testFiles.put(testFile.getName(), testFile);
+      }
+      return testFiles.build();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public ConformanceTestRunner(Class<?> clazz) throws InitializationError {
+    super(new TestClass(clazz));
+    Preconditions.checkArgument(ConformanceTests.class.equals(clazz));
+    testFiles = loadTestFiles();
+    testsToSkip =
+        ImmutableList.copyOf(
+            SPLITTER.splitToList(
+                System.getProperty("dev.cel.conformance.ConformanceTests.skip_tests")));
+  }
+
+  private boolean shouldSkipTest(String name) {
+    for (String testToSkip : testsToSkip) {
+      if (name.startsWith(testToSkip)) {
+        String consumedName = name.substring(testToSkip.length());
+        if (consumedName.isEmpty() || consumedName.startsWith("/")) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  @Override
+  protected List<ConformanceTest> getChildren() {
+    ArrayList<ConformanceTest> tests = new ArrayList<>();
+    for (SimpleTestFile testFile : testFiles.values()) {
+      for (SimpleTestSection testSection : testFile.getSectionList()) {
+        for (SimpleTest test : testSection.getTestList()) {
+          String name =
+              String.format("%s/%s/%s", testFile.getName(), testSection.getName(), test.getName());
+          tests.add(
+              new ConformanceTest(name, test, test.getDisableCheck() || shouldSkipTest(name)));
+        }
+      }
+    }
+    return tests;
+  }
+
+  @Override
+  protected Description describeChild(ConformanceTest child) {
+    return Description.createTestDescription(
+        ConformanceTest.class, child.getName(), ConformanceTest.class.getAnnotations());
+  }
+
+  @Override
+  protected void runChild(ConformanceTest child, RunNotifier notifier) {
+    Description desc = describeChild(child);
+    if (isIgnored(child)) {
+      notifier.fireTestIgnored(desc);
+    } else {
+      runLeaf(child, desc, notifier);
+    }
+  }
+
+  @Override
+  protected boolean isIgnored(ConformanceTest child) {
+    return child.shouldSkip();
+  }
+}
