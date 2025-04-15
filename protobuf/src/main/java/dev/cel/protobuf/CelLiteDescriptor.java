6e25ef4b883a08fc93843a01ed081f793dc42e30
Sokwhan Huh <sokwhan@google.com>
2025-04-15 15:57:47 -0700

    Drop support for java.lang.long uint support in CelValue
    PiperOrigin-RevId: 748055407

---
+++ protobuf/src/main/java/dev/cel/protobuf/CelLiteDescriptor.java
@@ -0,0 +1,302 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cel.protobuf;
+
+import static java.lang.Math.ceil;
+
+import com.google.errorprone.annotations.CanIgnoreReturnValue;
+import com.google.errorprone.annotations.Immutable;
+import com.google.protobuf.MessageLite;
+import dev.cel.common.annotations.Internal;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Supplier;
+
+/**
+ * Base class for code generated CEL lite descriptors to extend from.
+ *
+ * <p>CEL Library Internals. Do Not Use.
+ */
+@Internal
+@Immutable
+public abstract class CelLiteDescriptor {
+  @SuppressWarnings("Immutable") // Copied to unmodifiable map
+  private final Map<String, MessageLiteDescriptor> protoFqnToDescriptors;
+
+  private final String version;
+
+  public Map<String, MessageLiteDescriptor> getProtoTypeNamesToDescriptors() {
+    return protoFqnToDescriptors;
+  }
+
+  /** Retrieves the CEL-Java version this descriptor was generated with */
+  public String getVersion() {
+    return version;
+  }
+
+  /**
+   * Contains a collection of classes which describe protobuf messagelite types.
+   *
+   * <p>CEL Library Internals. Do Not Use.
+   */
+  @Internal
+  @Immutable
+  public static final class MessageLiteDescriptor {
+    private final String fullyQualifiedProtoTypeName;
+
+    @SuppressWarnings("Immutable") // Copied to an unmodifiable list
+    private final List<FieldLiteDescriptor> fieldLiteDescriptors;
+
+    @SuppressWarnings("Immutable") // Copied to an unmodifiable map
+    private final Map<String, FieldLiteDescriptor> fieldNameToFieldDescriptors;
+
+    @SuppressWarnings("Immutable") // Copied to an unmodifiable map
+    private final Map<Integer, FieldLiteDescriptor> fieldNumberToFieldDescriptors;
+
+    @SuppressWarnings("Immutable") // Does not alter the descriptor content
+    private final Supplier<MessageLite.Builder> messageBuilderSupplier;
+
+    public String getProtoTypeName() {
+      return fullyQualifiedProtoTypeName;
+    }
+
+    public List<FieldLiteDescriptor> getFieldDescriptors() {
+      return fieldLiteDescriptors;
+    }
+
+    public FieldLiteDescriptor getByFieldNameOrThrow(String fieldName) {
+      return Objects.requireNonNull(fieldNameToFieldDescriptors.get(fieldName));
+    }
+
+    public FieldLiteDescriptor getByFieldNumberOrThrow(int fieldNumber) {
+      return Objects.requireNonNull(fieldNumberToFieldDescriptors.get(fieldNumber));
+    }
+
+    /** Gets the builder for the message. Returns null for maps. */
+    public MessageLite.Builder newMessageBuilder() {
+      return messageBuilderSupplier.get();
+    }
+
+    /**
+     * CEL Library Internals. Do not use.
+     *
+     * <p>Public visibility due to codegen.
+     */
+    @Internal
+    public MessageLiteDescriptor(
+        String fullyQualifiedProtoTypeName,
+        List<FieldLiteDescriptor> fieldLiteDescriptors,
+        Supplier<MessageLite.Builder> messageBuilderSupplier) {
+      this.fullyQualifiedProtoTypeName = checkNotNull(fullyQualifiedProtoTypeName);
+      // This is a cheap operation. View over the existing map with mutators disabled.
+      this.fieldLiteDescriptors = Collections.unmodifiableList(checkNotNull(fieldLiteDescriptors));
+      Map<String, FieldLiteDescriptor> fieldNameMap =
+          new HashMap<>(getMapInitialCapacity(fieldLiteDescriptors.size()));
+      Map<Integer, FieldLiteDescriptor> fieldNumberMap =
+          new HashMap<>(getMapInitialCapacity(fieldLiteDescriptors.size()));
+      for (FieldLiteDescriptor fd : fieldLiteDescriptors) {
+        fieldNameMap.put(fd.fieldName, fd);
+        fieldNumberMap.put(fd.fieldNumber, fd);
+      }
+      this.fieldNameToFieldDescriptors = Collections.unmodifiableMap(fieldNameMap);
+      this.fieldNumberToFieldDescriptors = Collections.unmodifiableMap(fieldNumberMap);
+      this.messageBuilderSupplier = messageBuilderSupplier;
+    }
+  }
+
+  /**
+   * Describes a field of a protobuf messagelite type.
+   *
+   * <p>CEL Library Internals. Do Not Use.
+   */
+  @Internal
+  @Immutable
+  public static final class FieldLiteDescriptor {
+    private final int fieldNumber;
+    private final String fieldName;
+    private final JavaType javaType;
+    private final String fieldProtoTypeName;
+    private final Type protoFieldType;
+    private final CelFieldValueType celFieldValueType;
+    private final boolean isPacked;
+
+    /**
+     * Enumeration of the CEL field value type. This is analogous to the following from field
+     * descriptors:
+     *
+     * <ul>
+     *   <li>LIST: Repeated Field
+     *   <li>MAP: Map Field
+     *   <li>SCALAR: Neither of above (scalars, messages)
+     * </ul>
+     */
+    public enum CelFieldValueType {
+      SCALAR,
+      LIST,
+      MAP
+    }
+
+    /**
+     * Enumeration of the java type.
+     *
+     * <p>This is exactly the same as com.google.protobuf.Descriptors#JavaType
+     */
+    public enum JavaType {
+      INT,
+      LONG,
+      FLOAT,
+      DOUBLE,
+      BOOLEAN,
+      STRING,
+      BYTE_STRING,
+      ENUM,
+      MESSAGE
+    }
+
+    /**
+     * Enumeration of the protobuf type.
+     *
+     * <p>This is exactly the same as com.google.protobuf.Descriptors#Type
+     */
+    public enum Type {
+      DOUBLE,
+      FLOAT,
+      INT64,
+      UINT64,
+      INT32,
+      FIXED64,
+      FIXED32,
+      BOOL,
+      STRING,
+      GROUP,
+      MESSAGE,
+      BYTES,
+      UINT32,
+      ENUM,
+      SFIXED32,
+      SFIXED64,
+      SINT32,
+      SINT64
+    }
+
+    public String getFieldName() {
+      return fieldName;
+    }
+
+    /**
+     * Gets the field's java type.
+     *
+     * <p>This is exactly the same as com.google.protobuf.Descriptors#JavaType
+     */
+    public JavaType getJavaType() {
+      return javaType;
+    }
+
+    public CelFieldValueType getCelFieldValueType() {
+      return celFieldValueType;
+    }
+
+    /**
+     * Gets the field's protobuf type.
+     *
+     * <p>This is exactly the same as com.google.protobuf.Descriptors#Type
+     */
+    public Type getProtoFieldType() {
+      return protoFieldType;
+    }
+
+    /** Checks whether the repeated field is packed. */
+    public boolean getIsPacked() {
+      return isPacked;
+    }
+
+    /**
+     * Gets the fully qualified protobuf type name for the field, including its package name (ex:
+     * cel.expr.conformance.proto3.TestAllTypes.SingleStringWrapper). Returns an empty string for
+     * primitives.
+     */
+    public String getFieldProtoTypeName() {
+      return fieldProtoTypeName;
+    }
+
+    /**
+     * Must be public, used for codegen only. Do not use.
+     *
+     * @param fieldNumber Field index
+     * @param fieldName Name of the field
+     * @param javaType Canonical Java type name (ex: Long, Double, Float, Message... see
+     *     com.google.protobuf.Descriptors#JavaType)
+     * @param celFieldValueType Describes whether the field is a scalar, list or a map with respect
+     *     to CEL.
+     * @param protoFieldType Protobuf Field Type (ex: INT32, SINT32, GROUP, MESSAGE... see
+     *     com.google.protobuf.Descriptors#Type)
+     * @param fieldProtoTypeName Fully qualified protobuf type name for the field. Empty if the
+     *     field is a primitive.
+     */
+    @Internal
+    public FieldLiteDescriptor(
+        int fieldNumber,
+        String fieldName,
+        JavaType javaType,
+        CelFieldValueType celFieldValueType, // LIST, MAP, SCALAR
+        Type protoFieldType, // INT32, SINT32, GROUP, MESSAGE... (See Descriptors#Type)
+        boolean isPacked,
+        String fieldProtoTypeName) {
+      this.fieldNumber = fieldNumber;
+      this.fieldName = checkNotNull(fieldName);
+      this.javaType = javaType;
+      this.celFieldValueType = celFieldValueType;
+      this.protoFieldType = protoFieldType;
+      this.isPacked = isPacked;
+      this.fieldProtoTypeName = checkNotNull(fieldProtoTypeName);
+    }
+  }
+
+  protected CelLiteDescriptor(String version, List<MessageLiteDescriptor> messageInfoList) {
+    Map<String, MessageLiteDescriptor> protoFqnMap =
+        new HashMap<>(getMapInitialCapacity(messageInfoList.size()));
+    for (MessageLiteDescriptor msgInfo : messageInfoList) {
+      protoFqnMap.put(msgInfo.getProtoTypeName(), msgInfo);
+    }
+
+    this.version = version;
+    this.protoFqnToDescriptors = Collections.unmodifiableMap(protoFqnMap);
+  }
+
+  /**
+   * Returns a capacity that is sufficient to keep the map from being resized as long as it grows no
+   * larger than expectedSize and the load factor is ≥ its default (0.75).
+   */
+  private static int getMapInitialCapacity(int expectedSize) {
+    if (expectedSize < 3) {
+      return expectedSize + 1;
+    }
+
+    // See https://github.com/openjdk/jdk/commit/3e393047e12147a81e2899784b943923fc34da8e. 0.75 is
+    // used as a load factor.
+    return (int) ceil(expectedSize / 0.75);
+  }
+
+  @CanIgnoreReturnValue
+  private static <T> T checkNotNull(T reference) {
+    if (reference == null) {
+      throw new NullPointerException();
+    }
+    return reference;
+  }
+}
---
+++ protobuf/src/main/java/dev/cel/protobuf/ProtoDescriptorCollector.java
@@ -0,0 +1,193 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cel.protobuf;
+
+import static java.util.stream.Collectors.toCollection;
+
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.Descriptors;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Descriptors.FieldDescriptor.JavaType;
+import com.google.protobuf.Descriptors.FileDescriptor;
+import dev.cel.common.CelDescriptorUtil;
+import dev.cel.common.CelDescriptors;
+import dev.cel.common.internal.ProtoJavaQualifiedNames;
+import dev.cel.common.internal.WellKnownProto;
+import dev.cel.protobuf.CelLiteDescriptor.FieldLiteDescriptor;
+import dev.cel.protobuf.CelLiteDescriptor.FieldLiteDescriptor.CelFieldValueType;
+import dev.cel.protobuf.LiteDescriptorCodegenMetadata.FieldLiteDescriptorMetadata;
+import java.util.ArrayDeque;
+
+/**
+ * ProtoDescriptorCollector inspects a {@link FileDescriptor} to collect message information into
+ * {@link LiteDescriptorCodegenMetadata}. This is later utilized to create an instance of {@code
+ * MessageLiteDescriptor}.
+ */
+final class ProtoDescriptorCollector {
+
+  private final DebugPrinter debugPrinter;
+
+  ImmutableList<LiteDescriptorCodegenMetadata> collectCodegenMetadata(
+      FileDescriptor targetFileDescriptor) {
+    ImmutableList.Builder<LiteDescriptorCodegenMetadata> descriptorListBuilder =
+        ImmutableList.builder();
+    CelDescriptors celDescriptors =
+        CelDescriptorUtil.getAllDescriptorsFromFileDescriptor(
+            ImmutableList.of(targetFileDescriptor), /* resolveTypeDependencies= */ false);
+    ArrayDeque<Descriptor> descriptorQueue =
+        celDescriptors.messageTypeDescriptors().stream()
+            // Don't collect WKTs. They are included in the default descriptor pool.
+            .filter(d -> !WellKnownProto.getByTypeName(d.getFullName()).isPresent())
+            .collect(toCollection(ArrayDeque::new));
+
+    while (!descriptorQueue.isEmpty()) {
+      Descriptor descriptor = descriptorQueue.pop();
+      LiteDescriptorCodegenMetadata.Builder descriptorCodegenBuilder =
+          LiteDescriptorCodegenMetadata.newBuilder();
+      for (Descriptors.FieldDescriptor fieldDescriptor : descriptor.getFields()) {
+        FieldLiteDescriptorMetadata.Builder fieldDescriptorCodegenBuilder =
+            FieldLiteDescriptorMetadata.newBuilder()
+                .setFieldNumber(fieldDescriptor.getNumber())
+                .setFieldName(fieldDescriptor.getName())
+                .setIsPacked(fieldDescriptor.isPacked())
+                .setJavaType(adaptJavaType(fieldDescriptor.getJavaType()))
+                .setProtoFieldType(adaptFieldProtoType(fieldDescriptor.getType()));
+
+        switch (fieldDescriptor.getJavaType()) {
+          case ENUM:
+            fieldDescriptorCodegenBuilder.setFieldProtoTypeName(
+                fieldDescriptor.getEnumType().getFullName());
+            break;
+          case MESSAGE:
+            fieldDescriptorCodegenBuilder.setFieldProtoTypeName(
+                fieldDescriptor.getMessageType().getFullName());
+            break;
+          default:
+            break;
+        }
+
+        if (fieldDescriptor.isMapField()) {
+          fieldDescriptorCodegenBuilder.setCelFieldValueType(CelFieldValueType.MAP);
+          // Maps are treated as messages in proto.
+          // TODO: Directly embed key/value information within the field descriptor.
+          // This will further reduce descriptor binary size.
+          descriptorQueue.push(fieldDescriptor.getMessageType());
+        } else if (fieldDescriptor.isRepeated()) {
+          fieldDescriptorCodegenBuilder.setCelFieldValueType(CelFieldValueType.LIST);
+        } else {
+          fieldDescriptorCodegenBuilder.setCelFieldValueType(CelFieldValueType.SCALAR);
+        }
+
+        descriptorCodegenBuilder.addFieldDescriptor(fieldDescriptorCodegenBuilder.build());
+
+        debugPrinter.print(
+            String.format(
+                "Collecting message %s, for field %s, type: %s",
+                descriptor.getFullName(),
+                fieldDescriptor.getFullName(),
+                fieldDescriptor.getType()));
+      }
+
+      descriptorCodegenBuilder.setProtoTypeName(descriptor.getFullName());
+      // Maps are resolved as an actual Java map, and doesn't have a MessageLite.Builder associated.
+      if (!descriptor.getOptions().getMapEntry()) {
+        String sanitizedJavaClassName =
+            ProtoJavaQualifiedNames.getFullyQualifiedJavaClassName(descriptor).replace('$', '.');
+        descriptorCodegenBuilder.setJavaClassName(sanitizedJavaClassName);
+      }
+
+      descriptorListBuilder.add(descriptorCodegenBuilder.build());
+    }
+
+    return descriptorListBuilder.build();
+  }
+
+  static ProtoDescriptorCollector newInstance(DebugPrinter debugPrinter) {
+    return new ProtoDescriptorCollector(debugPrinter);
+  }
+
+  private static FieldLiteDescriptor.Type adaptFieldProtoType(
+      Descriptors.FieldDescriptor.Type type) {
+    switch (type) {
+      case DOUBLE:
+        return FieldLiteDescriptor.Type.DOUBLE;
+      case FLOAT:
+        return FieldLiteDescriptor.Type.FLOAT;
+      case INT64:
+        return FieldLiteDescriptor.Type.INT64;
+      case UINT64:
+        return FieldLiteDescriptor.Type.UINT64;
+      case INT32:
+        return FieldLiteDescriptor.Type.INT32;
+      case FIXED64:
+        return FieldLiteDescriptor.Type.FIXED64;
+      case FIXED32:
+        return FieldLiteDescriptor.Type.FIXED32;
+      case BOOL:
+        return FieldLiteDescriptor.Type.BOOL;
+      case STRING:
+        return FieldLiteDescriptor.Type.STRING;
+      case MESSAGE:
+        return FieldLiteDescriptor.Type.MESSAGE;
+      case BYTES:
+        return FieldLiteDescriptor.Type.BYTES;
+      case UINT32:
+        return FieldLiteDescriptor.Type.UINT32;
+      case ENUM:
+        return FieldLiteDescriptor.Type.ENUM;
+      case SFIXED32:
+        return FieldLiteDescriptor.Type.SFIXED32;
+      case SFIXED64:
+        return FieldLiteDescriptor.Type.SFIXED64;
+      case SINT32:
+        return FieldLiteDescriptor.Type.SINT32;
+      case SINT64:
+        return FieldLiteDescriptor.Type.SINT64;
+      case GROUP:
+        return FieldLiteDescriptor.Type.GROUP;
+    }
+
+    throw new IllegalArgumentException("Unknown Type: " + type);
+  }
+
+  private static FieldLiteDescriptor.JavaType adaptJavaType(JavaType javaType) {
+    switch (javaType) {
+      case INT:
+        return FieldLiteDescriptor.JavaType.INT;
+      case LONG:
+        return FieldLiteDescriptor.JavaType.LONG;
+      case FLOAT:
+        return FieldLiteDescriptor.JavaType.FLOAT;
+      case DOUBLE:
+        return FieldLiteDescriptor.JavaType.DOUBLE;
+      case BOOLEAN:
+        return FieldLiteDescriptor.JavaType.BOOLEAN;
+      case STRING:
+        return FieldLiteDescriptor.JavaType.STRING;
+      case BYTE_STRING:
+        return FieldLiteDescriptor.JavaType.BYTE_STRING;
+      case ENUM:
+        return FieldLiteDescriptor.JavaType.ENUM;
+      case MESSAGE:
+        return FieldLiteDescriptor.JavaType.MESSAGE;
+    }
+
+    throw new IllegalArgumentException("Unknown JavaType: " + javaType);
+  }
+
+  private ProtoDescriptorCollector(DebugPrinter debugPrinter) {
+    this.debugPrinter = debugPrinter;
+  }
+}
