9313ea9532327517eaf71d92c3c2ed132fd54bf8
Chinmay Madeshi <madeshic@google.com>
2025-05-07 11:54:16 -0700

    Internal change
    PiperOrigin-RevId: 755946795

---
+++ testing/src/main/java/dev/cel/testing/utils/ProtoDescriptorUtils.java
@@ -0,0 +1,91 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package dev.cel.testing.utils;
+
+import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+import static dev.cel.testing.utils.ClassLoaderUtils.loadDescriptors;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.Files;
+import com.google.protobuf.DescriptorProtos.FileDescriptorSet;
+import com.google.protobuf.Descriptors.Descriptor;
+import com.google.protobuf.Descriptors.FileDescriptor;
+import com.google.protobuf.ExtensionRegistry;
+import com.google.protobuf.Message;
+import dev.cel.common.CelDescriptorUtil;
+import dev.cel.common.CelDescriptors;
+import dev.cel.common.internal.DefaultInstanceMessageFactory;
+import java.io.File;
+import java.io.IOException;
+import java.util.NoSuchElementException;
+
+/** Utility class for working with proto descriptors. */
+public final class ProtoDescriptorUtils {
+
+  /**
+   * Returns all the descriptors from the JVM.
+   *
+   * @return The {@link CelDescriptors} object containing all the descriptors.
+   * @throws IOException If there is an error during the loading process.
+   */
+  public static CelDescriptors getAllDescriptors() throws IOException {
+    ImmutableList<Descriptor> compileTimeLoadedDescriptors = loadDescriptors();
+    FileDescriptorSet fileDescriptorSet =
+        getFileDescriptorSet(System.getProperty("file_descriptor_set_path"));
+    ImmutableSet<String> runtimeFileDescriptorNames =
+        CelDescriptorUtil.getFileDescriptorsFromFileDescriptorSet(fileDescriptorSet).stream()
+            .map(FileDescriptor::getFullName)
+            .collect(toImmutableSet());
+
+    // Get all the file descriptors from the descriptors which are loaded from the JVM and use the
+    // ones which match the ones provided by the user in the file descriptor set.
+    ImmutableList<FileDescriptor> userProvidedFileDescriptors =
+        CelDescriptorUtil.getFileDescriptorsForDescriptors(compileTimeLoadedDescriptors).stream()
+            .filter(
+                fileDescriptor -> runtimeFileDescriptorNames.contains(fileDescriptor.getFullName()))
+            .collect(toImmutableList());
+
+    // Get all the descriptors from the file descriptors above which include nested, extension and
+    // message type descriptors as well.
+    return CelDescriptorUtil.getAllDescriptorsFromFileDescriptor(userProvidedFileDescriptors);
+  }
+
+  /**
+   * Returns the default instance of the given descriptor.
+   *
+   * @param descriptor The descriptor to get the default instance for.
+   * @return The default instance of the given descriptor.
+   * @throws IOException If there is an error during the loading process.
+   */
+  public static Message getDefaultInstance(Descriptor descriptor) throws IOException {
+    return DefaultInstanceMessageFactory.getInstance()
+        .getPrototype(descriptor)
+        .orElseThrow(
+            () ->
+                new NoSuchElementException(
+                    "Could not find a default message for: " + descriptor.getFullName()));
+  }
+
+  private static FileDescriptorSet getFileDescriptorSet(String fileDescriptorSetPath)
+      throws IOException {
+    // We can pass an empty extension registry here because extensions are recovered later when
+    // creating the extension registry in {@link #createExtensionRegistry}.
+    return FileDescriptorSet.parseFrom(
+        Files.toByteArray(new File(fileDescriptorSetPath)), ExtensionRegistry.newInstance());
+  }
+
+  private ProtoDescriptorUtils() {}
+}
