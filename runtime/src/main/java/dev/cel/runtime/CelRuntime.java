a06af0c72442ec8b4c040db5c84f8ac67cc1e127
Dmitri Plotnikov <dplotnikov@google.com>
2025-07-25 15:10:55 -0700

    Remove CelRuntime dependency on CelLiteRuntime.
    This provides a clean dependency separation: downstream build targets depend either on the full or lite protobuf library, but not both.
    
    PiperOrigin-RevId: 787255311

--- runtime/src/main/java/dev/cel/runtime/CelRuntime.java
+++ runtime/src/main/java/dev/cel/runtime/CelRuntime.java
@@ -37,7 +37,20 @@ public interface CelRuntime {
 
   /** Creates an evaluable {@code Program} instance which is thread-safe and immutable. */
   @Immutable
-  interface Program extends CelLiteRuntime.Program {
+  interface Program {
+
+    /** Evaluate the expression without any variables. */
+    Object eval() throws CelEvaluationException;
+
+    /** Evaluate the expression using a {@code mapValue} as the source of input variables. */
+    Object eval(Map<String, ?> mapValue) throws CelEvaluationException;
+
+    /**
+     * Evaluate a compiled program with {@code mapValue} and late-bound functions {@code
+     * lateBoundFunctionResolver}.
+     */
+    Object eval(Map<String, ?> mapValue, CelFunctionResolver lateBoundFunctionResolver)
+        throws CelEvaluationException;
 
     /** Evaluate the expression using {@code message} fields as the source of input variables. */
     Object eval(Message message) throws CelEvaluationException;
