b3612d4cbfed981b5195e47d2291bd1e610059cc
Sokwhan Huh <sokwhan@google.com>
2025-12-14 17:40:22 -0800

    Introduce CEL exception classes that correspond to canonical error codes
    PiperOrigin-RevId: 844506576

---
+++ common/src/main/java/dev/cel/common/exceptions/CelDivideByZeroException.java
@@ -0,0 +1,32 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dev.cel.common.exceptions;
+
+import dev.cel.common.CelErrorCode;
+import dev.cel.common.CelRuntimeException;
+import dev.cel.common.annotations.Internal;
+
+/** Indicates that a division by zero occurred. */
+@Internal
+public final class CelDivideByZeroException extends CelRuntimeException {
+
+  public CelDivideByZeroException() {
+    super("/ by zero", CelErrorCode.DIVIDE_BY_ZERO);
+  }
+
+  public CelDivideByZeroException(Throwable cause) {
+    super(cause, CelErrorCode.DIVIDE_BY_ZERO);
+  }
+}
